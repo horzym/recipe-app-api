031a2bbc4247d9dc9f7b1c5e54c661cff8e0fbae
horzym <wojtektok1996@gmail.com>
2019-08-20 14:25:16 +0100

    Costumize admin model and admin site

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,20 @@
-# from django.contrib import admin
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from core import models
+from django.utils.translation import gettext as _
 
-# Register your models here.
+class UserAdmin(BaseUserAdmin):
+    ordering = ['id']
+    list_display = ['name', 'email']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal info'), {'fields': ('name',)}),
+        (_('Permissions'), {'fields':('is_staff','is_superuser','is_active')}),
+        (_('Important dates'), {'fields':('last_login',)})
+    )
+    add_fieldsets = (
+        (None, {'classes':('wide',),
+                'fields':('email','password1','password2')}),
+    )
+
+admin.site.register(models.User, UserAdmin)
