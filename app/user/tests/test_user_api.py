f2dcccaf459c9e70fb34233ff72701ab04a12b30
horzym <wojtektok1996@gmail.com>
2019-08-23 18:12:38 +0100

    API added manage user ednpoint.

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -8,6 +8,8 @@ from rest_framework import status
 
 CREATE_USER_URL = reverse('user:create')
 TOKEN_URL = reverse('user:token')
+ME_URL = reverse('user:me')
+
 
 def create_user(**params):
     return get_user_model().objects.create_user(**params)
@@ -56,7 +58,7 @@ class PublicUserApiTest(TestCase):
 
     def test_token_for_user(self):
         """User get token creat user"""
-        payload = {'email': 'correct@email.com', 'password': 'correct_pasword123'}
+        payload = {'email': 'email@email.com', 'password': 'pasword123'}
         create_user(**payload)
         res = self.client.post(TOKEN_URL, payload)
 
@@ -64,8 +66,8 @@ class PublicUserApiTest(TestCase):
         self.assertIn('token', res.data)
 
     def test_token_if_invalid_credentials(self):
-        create_user(email = 'test@emaail.com', password = 'testpass')
-        payload = {'email': 'email@email.com', 'password': 'xddsas'}
+        create_user(email='test@emaail.com', password='testpass')
+        payload = {'email': 'mail@email.pl', 'password': 'passs'}
 
         res = self.client.post(TOKEN_URL, payload)
 
@@ -74,9 +76,55 @@ class PublicUserApiTest(TestCase):
 
     def test_token_if_missing_fields(self):
         """Test get token if user miss fill one fields"""
-        create_user(email='test@test.com', password='passssword')
-        payload = { 'email': '', 'password': 'pass123'}
+        create_user(email='test@test.com', password='password')
+        payload = {'email': '', 'password': 'pass123'}
         res = self.client.post(TOKEN_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertNotIn('token', res.data)
+
+    def test_retrieve_user_unauthorized(self):
+        """Test that authentication required for users"""
+        res = self.client.get(ME_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class PrivateUserApiTests(TestCase):
+    """Test API requests that require authentication"""
+
+    def setUp(self):
+        self.user = create_user(
+            email='test@londonappdev.com',
+            password='testpass',
+            name='fname',
+        )
+        self.client = APIClient()
+        self.client.force_authenticate(user=self.user)
+
+    def test_retrieve_profile_success(self):
+        """Test retrieving profile for logged in user"""
+        res = self.client.get(ME_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, {
+            'name': self.user.name,
+            'email': self.user.email,
+        })
+
+    def test_post_me_not_allowed(self):
+        """Test that POST is not allowed on the me URL"""
+        res = self.client.post(ME_URL, {})
+
+        self.assertEqual(res.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def test_update_user_profile(self):
+        """Test updating the user profile for authenticated user"""
+        payload = {'name': 'new name', 'password': 'newpassword123'}
+
+        res = self.client.patch(ME_URL, payload)
+
+        self.user.refresh_from_db()
+        self.assertEqual(self.user.name, payload['name'])
+        self.assertTrue(self.user.check_password(payload['password']))
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
--- app/user/views.py
+++ app/user/views.py
@@ -1,14 +1,25 @@
 from user.serializers import UserSerializers, AuthTokenSerializer
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
-from rest_framework import generics
+from rest_framework import generics, permissions, authentication
 
 
 class CreateUserView(generics.CreateAPIView):
     """Create a new user in the system"""
     serializer_class = UserSerializers
 
+
 class CreateTokenView(ObtainAuthToken):
     """Creat a new auth for user"""
     serializer_class = AuthTokenSerializer
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    """Manage the authenticated user"""
+    serializer_class = UserSerializers
+    authentication_classes = (authentication.TokenAuthentication,)
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get_object(self):
+        return self.request.user
