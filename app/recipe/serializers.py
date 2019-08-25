411131c26e26c6f4fe2d9cbaed53c2abe21a4358
horzym <wojtektok1996@gmail.com>
2019-08-25 19:06:21 +0100

    Added detail to recipe.

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -30,9 +30,14 @@ class RecipeSerializer(serializers.ModelSerializer):
         queryset=Tag.objects.all()
     )
 
-
     class Meta:
         model = Recipe
         fields = ('id', 'title', 'time_minutes', 'price',
                   'ingredients', 'tags', 'link')
         read_only_fields = ('id',)
+
+
+class RecipeDetailSerializer(RecipeSerializer):
+    """Serializer for detail to recipe"""
+    ingredients = IngredientSerializer(many=True, read_only=True)
+    tags = TagSerializer(many=True, read_only=True)
